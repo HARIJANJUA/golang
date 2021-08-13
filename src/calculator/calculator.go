2b268d9bd019130fe3c77a1c1b7b1fb3f0dd221d
Hari Janjua <harij@mkclindia.local>
2021-08-13 15:35:42 +0530

    pushed

---
+++ src/calculator/calculator.go
@@ -0,0 +1,24 @@
+package calculator
+
+import "fmt"
+
+func Calc() {
+	fmt.Println("hello")
+	// var a, b int
+	// 	fmt.Println("Enter first number:")
+	// fmt.Scan(&a)
+	// fmt.Println("Enter second number:")
+	// fmt.Scan(&b)
+
+	// fmt.Println("1. Add")
+	// 	fmt.Println("2. Subtract")
+	// fmt.Scan(&choice)
+
+	// var ans int
+	// if (choice == 1){
+	// 	ans = a+b
+	// }else if (choice == 2){
+	// 	ans = a-b
+	// }
+	// fmt.Println("Result: ", ans)
+}
---
+++ src/tem/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"calculator"
+)
+
+func main() {
+	calculator.Calc()
+}
